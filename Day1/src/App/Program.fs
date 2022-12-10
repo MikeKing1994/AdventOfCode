28606580a85f643dc6da62d9f05b19d4a535507e
Michael King <michaelj.king@btinternet.com>
2022-12-10 17:21:32 +0000

    Day 1

---
+++ Day1/src/App/Program.fs
@@ -0,0 +1,28 @@
+﻿namespace Project
+module Program = 
+    open System
+
+    let split = Calories.input.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries)
+    
+    let tryParseInt (i: string) = 
+        match Int32.TryParse i with 
+        | true, parsed -> parsed 
+        | _ -> 0
+
+    let toCalorieCount elf = 
+        elf 
+        |> Array.map tryParseInt
+        |> Array.sum
+
+    let splitPerElf (elf: string) = 
+        elf.Split("\r\n")
+
+    split 
+    |> Array.where (fun elf -> String.IsNullOrWhiteSpace(elf) |> not)
+    |> Array.map splitPerElf
+    |> Array.map toCalorieCount
+    |> Array.sortDescending
+    |> Array.take 3
+    |> Array.sum
+    |> printfn "%i"
+
